2ddaf4fae38295f4c91d922ec8bc86787882777b
Ståle Dahl <stalehd@gmail.com>
2023-02-03 14:13:23 +0100

    Package with stadnard gateway property names

---
+++ pkg/stdgw/doc.go
@@ -0,0 +1,3 @@
+// Package stdgw contains defaults for commonly-known gateway types. These constants are used to keep
+// the field names across projects.
+package stdgw
---
+++ pkg/stdgw/lora.go
@@ -0,0 +1,21 @@
+package stdgw
+
+// Configuration parameters for LoRa gateways
+const (
+	LoraApplicationEUI = "appEui"
+	LoraDeviceEUI      = "devEui"
+	LoraState          = "state"
+	LoraDevAddr        = "devAddr"
+	LoraAppKey         = "appKey"
+	LoraAppSKey        = "appSKey"
+	LoraNwkSKey        = "nwkSKey"
+	LoraFCntUp         = "fCntUp"
+	LoraFCntDn         = "fCntDn"
+	LoraRelaxedCounter = "relaxedCounter"
+	LoraKeyWarning     = "keyWarning"
+	LoraGatewayEUI     = "gatewayEui"
+	LoraRSSI           = "rssi"
+	LoraSNR            = "snr"
+	LoraFrequency      = "frequency"
+	LoraDataRate       = "dataRate"
+)
